b53b8f5d463cc1f52a0a766d48db8aa352022071
hwoo4567 <hwoo4567@email.com>
2024-07-16 23:47:45 +0800

    Day 2 (testing streamlit)

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
-# For anaconda installation test
-# Delete it later
+import streamlit as st
+from PIL import Image
 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+def main():
+    st.title('Display Two Images')
 
-class MyWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+    col1, col2 = st.columns(2)
 
-        self.setWindowTitle('Example')
-        self.setGeometry(100, 100, 400, 300)
+    # Displaying first image in the first column
+    with col1:
+        st.header('First Image')
+        image1 = Image.open('t1.jpg')
+        st.image(image1, use_column_width=True)
+
+    # Displaying second image in the second column
+    with col2:
+        st.header('Second Image')
+        image2 = Image.open('t2.jpg')
+        st.image(image2, use_column_width=True)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    sys.exit(app.exec_())
+    main()
